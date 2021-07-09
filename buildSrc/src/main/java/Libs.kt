8dc0844f6b6c85d358a1b1db7514ac32ab32c084
Irvan Mlambo <mlamboirvin8@gmail.com>
2021-07-09 17:38:10 +0200

    Fixed buidSrc module

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,13 @@
+object Libs {
+
+    const val APPCOMPAT = "androidx.appcompat:appcompat"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout"
+    const val CORE_KTX = "androidx.core:core-ktx"
+    const val LIFECYCLE_EXTENSION = "androidx.lifecycle:lifecycle-extensions"
+    const val LIFECYCLE_RUNTIME_KTX = "androidx.lifecycle:lifecycle-runtime-ktx"
+    const val LIFECYCLE_VIEW_MODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx"
+    const val MATERIAL = "com.google.android.material:material"
+    const val PLAY_SERVICES_LOCATION = "com.google.android.gms:play-services-location"
+    const val RETROFIT = "com.squareup.retrofit2:retrofit"
+    const val RETROFIT_CONVERTER_GSON = "com.squareup.retrofit2:converter-gson"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+
+    const val COMPILE_SDK = 30
+    const val TARGET_SDK = 30
+    const val MIN_SDK = 23
+
+    const val ANDROID_GRADLE_PLUGIN = "7.1.0-alpha02"
+    const val KOTLIN_PLUGIN = "1.5.20"
+}
